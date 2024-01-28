3aa14113374d07857a9969838400c62951cf6c99
Sanny <delta.mosh.services@gmail.com>
2024-01-28 17:27:53 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import 'regenerator-runtime/runtime';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import botGif from './assets/bot-gif.gif';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [joke, setJoke] = useState({ setup: null, punchline: null });
+  const [loading, setLoading] = useState(false);
+  const synth = window.speechSynthesis;
+  const voice = synth.getVoices();
+  const getJoke = async () => {
+    setLoading(true);
+    const { data } = await axios.get(
+      'https://official-joke-api.appspot.com/random_joke'
+    );
+    let newJoke = {
+      setup: data.setup,
+      punchline: data.punchline,
+    };
+    setJoke({ ...newJoke });
+    let temp = `${data.setup}...............${data.punchline}`;
+    const utterThis = new SpeechSynthesisUtterance(temp);
+    utterThis.voice = voice[4];
+    utterThis.rate = 0.85;
+    utterThis.pitch = 0.5;
+    utterThis.volume = 0.75;
+    utterThis.lang = 'en-US';
+    synth.speak(utterThis);
+    setLoading(false);
+  };
   return (
     <>
-    <div>Hello, Welcome to Joke Telling Bot</div>
+      <div className='vh-100 d-flex justify-content-center align-items-center'>
+        <div className="text-center">
+            <p>{joke.setup}</p>
+          <div className='mb-4'>
+            <img
+              className="img-fluid mb-2"
+              src={ botGif }
+              alt="joke robot"
+            />
+            <p>{joke.punchline}</p>
+          </div>
+          <button
+            type="button"
+            className="btn btn-sm btn-outline-primary"
+            onClick={getJoke}
+          >
+            {loading && (
+              <span
+                className="spinner-border spinner-border-sm"
+                role="status"
+                aria-hidden="true"
+              ></span>
+            )}
+            {!loading && `TELL ME A JOKE`}
+          </button>
+        </div>
+      </div>
     </>
   )
 }
